5c333f1172f78dc1b9563565c68c4b9be5a9d9c3
lsinko <lsinko23@student.foi.hr>
2025-07-19 03:56:59 +0200

    Add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://trueallyguide.com',
+  generateRobotsTxt: true,      // /robots.txt
+  sitemapSize: 7000,            // defaults are fine; change if huge
+};
