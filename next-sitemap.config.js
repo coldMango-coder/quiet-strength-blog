fe1a78bd4dbbedd11b84312d8592cdca1063815a
lsinko <lsinko23@student.foi.hr>
2025-07-20 17:37:51 +0200

    Point sitemap at www.trueallyguide.com

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://trueallyguide.com',
+  siteUrl: 'https://www.trueallyguide.com',
   generateRobotsTxt: true,      // /robots.txt
   sitemapSize: 7000,            // defaults are fine; change if huge
 };
